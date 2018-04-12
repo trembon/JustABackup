34c3210469948f85d40f1b1c228ac1490bbbaf7e
trembon <trembon@hotmail.com>
2018-04-12 21:24:12 +0200

    updated the design on the scheduled job details page

--- JustABackup.Models/Job/JobDetailModel.cs
+++ JustABackup.Models/Job/JobDetailModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,14 +12,18 @@ namespace JustABackup.Models.Job
 
         public string Name { get; set; }
 
+        [Display(Name = "Schedule")]
         public string CronSchedule { get; set; }
 
         public bool HasChangedModel { get; set; }
 
+        [Display(Name = "Backup Provider")]
         public string BackupProvider { get; set; }
 
+        [Display(Name = "Storage Provider")]
         public string StorageProvider { get; set; }
 
+        [Display(Name = "Transform Providers")]
         public IEnumerable<string> TransformProviders { get; set; }
 
         public JobDetailModel()
---
+++ JustABackup/TagHelpers/DisplayForTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JustABackup.TagHelpers
+{
+    [HtmlTargetElement("p", Attributes = ForAttributeName)]
+    public class DisplayForTagHelper : TagHelper
+    {
+        private const string ForAttributeName = "asp-for";
+
+        [HtmlAttributeName(ForAttributeName)]
+        public ModelExpression For { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            if (output == null)
+                throw new ArgumentNullException(nameof(output));
+
+            var text = For.ModelExplorer.GetSimpleDisplayText();
+            output.Content.SetContent(text);
+        }
+    }
+}
